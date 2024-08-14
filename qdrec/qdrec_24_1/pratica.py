674f67de448e9d68e4900906460ba32038a07a4c
Bernardo Costa <bemcdc@gmail.com>
2024-08-14 19:19:18 -0300

    refactor: extract find act code to a function + run for designations

--- qdrec/qdrec_24_1/pratica.py
+++ qdrec/qdrec_24_1/pratica.py
@@ -10,6 +10,32 @@ script_path = os.path.dirname(os.path.realpath(__file__))
 file_path = os.path.join("samples", "DiarioOficial_NovaIguaçu_0518-24.pdf")
 
 
+def find_act(word: str, full_text: str):
+    act_indexes = [m.start() for m in re.finditer(word, full_text.lower())]
+
+    for start_index in act_indexes:
+        print("----")
+
+        # Find text from the word to the next full stop
+        end_index = extracted_text.find(".", start_index)
+        act = extracted_text[start_index : end_index + 1]
+        print(act)
+
+        # Find the first ocurrence of an uppercase letter after the word, up to the next comma
+        # This is the name of the person affected by the act
+        rest = act[act.lower().find(word) + len(word) :].strip()
+
+        name_start_index = re.search("[A-Z]", rest)
+        if name_start_index is None:
+            continue
+
+        name_start_index = name_start_index.start()
+        name_end_index = rest.find(",", name_start_index)
+        name = rest[name_start_index:name_end_index]
+        name = name.strip().replace("\n", "").replace(" -", "")
+        print("NAME:", name)
+
+
 all_text = ""
 
 with open(file_path, "rb") as file:
@@ -20,42 +46,8 @@ with open(file_path, "rb") as file:
         print(page_number, len(extracted_text))
         print("----")
 
-        processed_text = extracted_text.lower()
-
-        # Find position of the word 'nomear' in the text
-        designations = [m.start() for m in re.finditer("nomear", processed_text)]
-
-        for start_index in designations:
-            # Find text from the word 'nomear' to the next full stop
-            end_index = extracted_text.find(".", start_index)
-            designation_act = extracted_text[start_index : end_index + 1]
-            print(designation_act)
-
-        dismissals = [m.start() for m in re.finditer("exonerar", processed_text)]
-
-        for start_index in dismissals:
-            print("----")
-
-            # Find text from the word 'exonerar' to the next full stop
-            end_index = extracted_text.find(".", start_index)
-            dismissal_act = extracted_text[start_index : end_index + 1]
-            print(dismissal_act)
-
-            # Find the first ocurrence of an uppercase letter after the word 'exonerar', up to the next comma
-            # This is the name of the person being dismissed
-            rest = dismissal_act[
-                dismissal_act.lower().find("exonerar") + len("exonerar") :
-            ].strip()
-            print(rest)
-            name_start_index = re.search("[A-Z]", rest)
-            if name_start_index is None:
-                continue
-            print(name_start_index)
-            name_start_index = name_start_index.start()
-            name_end_index = rest.find(",", name_start_index)
-            dismissed_name = rest[name_start_index:name_end_index]
-            dismissed_name = dismissed_name.strip().replace("\n", "").replace(" -", "")
-            print("NAME:", dismissed_name)
+        find_act("nomear", extracted_text)
+        find_act("exonerar", extracted_text)
 
         all_text = all_text + extracted_text
 
